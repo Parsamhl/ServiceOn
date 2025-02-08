ba1207a92597467328c61c5e8c9392864a3fb664
parsa <mparsah$@gmail.com>
2025-02-08 17:41:30 +0330

    EndPoint MVC project Created

---
+++ ServiceOn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServiceOn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
