f630c378fafc0b449b9007e1809c29401203331d
parsa <mparsah$@gmail.com>
2025-02-14 00:45:56 +0330

    Repositories Created

--- ServiceOn/Program.cs
+++ ServiceOn/Program.cs
@@ -1,8 +1,34 @@
+using App.Domain.Core.ServiceOn.Address.Data.Repository;
+using App.Domain.Core.ServiceOn.AllService.Data.Repository.cs;
+using App.Domain.Core.ServiceOn.Category.Data.Repository;
+using App.Domain.Core.ServiceOn.FeedBack.Data.Repository;
+using App.Domain.Core.ServiceOn.Orders.Data.Repository;
+using App.Domain.Core.ServiceOn.User.Data.Repository;
+using App.Infra.Repository.Ef.ServiceOn.Address;
+using App.Infra.Repository.Ef.ServiceOn.Allservice;
+using App.Infra.Repository.Ef.ServiceOn.Category;
+using App.Infra.Repository.Ef.ServiceOn.Costomer;
+using App.Infra.Repository.Ef.ServiceOn.CostomerRequest;
+using App.Infra.Repository.Ef.ServiceOn.Expert;
+using App.Infra.Repository.Ef.ServiceOn.ExpertRequest;
+using App.Infra.Repository.Ef.ServiceOn.FeedBack;
+using App.Infra.Repository.Ef.ServiceOn.Operator;
+using App.Infra.Repository.Ef.ServiceOn.SubCategories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped<ICostomerRepository, CostomerRepository>();
+builder.Services.AddScoped<IExpertrepository , ExpertRepository>();
+builder.Services.AddScoped<IOperatorRepository , OperatorRepository>();
+builder.Services.AddScoped<ICostomerRequestRepository , CostomerRequserRepository>();
+builder.Services.AddScoped<IExpertRequestRepository , ExpertRequestRepository>();
+builder.Services.AddScoped<ICategoryRepository , CategoryRepository>();
+builder.Services.AddScoped<ISubcategoryRepository , SubCategoryRepository>();
+builder.Services.AddScoped<IAllServicerepository,AllServicerepository>();
+builder.Services.AddScoped<IFeedBackRepository , FeedBackRepository>();
+builder.Services.AddScoped<IAddressRepository , AddressRepository>();
 var app = builder.Build();
 
 
